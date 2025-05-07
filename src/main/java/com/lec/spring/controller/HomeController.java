0c661490d68da2e3b93d1d9b980c6b3c5f7db86c
hjlee <lhj020731@gmail.com>
2025-05-07 10:04:24 +0900

    init

---
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.lec.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j  // Logger 자동 초기화
+@RestController // controller + responsebody -> body 리턴 (cf. controller : view 리턴)
+public class HomeController {
+
+	@GetMapping("/aws/v1")
+	public String hello(@RequestParam(defaultValue = "1") Integer num) {
+		
+		// 페이지 요청할 때마다 로그 출력 -> 보통 파일에 저장
+		if (num == 1) log.info("💚 /aws/v1 호출. info 로그");
+		else if (num == -1) log.error("💛 /aws/v1 호출. error 로그");
+		else if (num == 0) log.warn("❤️ /aws/v1 호출. warn 로그");
+		
+		return "<h1>AWS 배포 v1</h1>";
+		
+	}
+
+}
---
+++ src/test/java/com/lec/spring/controller/HomeControllerTest.java
@@ -0,0 +1,29 @@
+package com.lec.spring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// 서버 동작 시, 랜덤 포트로 동작하도록 설정
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HomeControllerTest {
+	
+	// 테스트 시에서만 사용하는 Bean (자동 생성됨)
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	void hello() {
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("/aws/v1", String.class);
+		
+		// 예상결과값(매개변수1)과 실제결과값(매개변수2)가 같을 경우에만 테스트 성공 (테스트 시에만 사용 가능한 함수)
+		assertEquals("<h1>AWS 배포 v1</h1>", response.getBody());
+		
+	}
+	
+}
